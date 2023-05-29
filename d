3da3eb247400d666fda5e9494da418ef98948bc6
Lunateka1d <bruna.marques@ubi.pt>
2023-05-29 18:18:39 +0100

    Guarda na BD quais são as checkbox que estão checked

---
+++ d
@@ -0,0 +1,29 @@
+[33mcommit f91202fd501c20bfa3a9b250c4fa2525b85defde[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Lunateka1d <bruna.marques@ubi.pt>
+Date:   Mon May 29 17:57:40 2023 +0100
+
+    To-Do list a funcionar corretamente
+
+[33mcommit b9bd0fcc55ea7ba7a64355f0cb4520ac7310a4ad[m
+Author: Lunateka1d <bruna.marques@ubi.pt>
+Date:   Mon May 29 17:43:52 2023 +0100
+
+    Design Responsivo
+
+[33mcommit f9bab395777153647efa2066cda8ec86534c28db[m
+Author: Lunateka1d <bruna.marques@ubi.pt>
+Date:   Mon May 29 17:36:27 2023 +0100
+
+    To-Do List quase feita
+
+[33mcommit 46a829f36e9b6ced4182ce793adf4bcbe9ffe680[m
+Author: Lunateka1d <bruna.marques@ubi.pt>
+Date:   Mon May 29 14:41:14 2023 +0100
+
+    nome dos ficheiros js alterados
+
+[33mcommit e4dffbb07eee45f5da734718c4b38aa12e28b965[m
+Author: Lunateka1d <bruna.marques@ubi.pt>
+Date:   Mon May 29 10:26:40 2023 +0100
+
+    versão inicial - 29/05/2023
--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-import { getFirestore, collection, addDoc, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, where, updateDoc,  doc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -30,46 +30,75 @@ const form = document.querySelector('form');
 // Definir um conjunto para armazenar os IDs dos documentos exibidos
 const displayedTasks = new Set();
 
+// Função para exibir as tarefas
+function displayTasks() {
+  const user = auth.currentUser;
+  const userId = user.uid;
+  const userTasksRef = collection(db, "users");
+  const tasksQuery = query(userTasksRef, where("userId", "==", userId));
+
+  // Limpar a lista antes de exibir as tarefas novamente
+  list.innerHTML = "";
+
+  onSnapshot(tasksQuery, (snapshot) => {
+    snapshot.forEach((doc1) => {
+      const taskId = doc1.id;
+      const taskData = doc1.data();
+
+      if (!displayedTasks.has(taskId)) {
+        const listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+
+        const checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.setAttribute('id', taskId);
+        checkbox.checked = taskData.completed;
+
+        // Adicionar o ouvinte de eventos ao evento 'change' da checkbox
+        checkbox.addEventListener('change', (event) => {
+          const checked = event.target.checked;
+          const taskId = event.target.id;
+
+          const taskRef = doc(db, "users", taskId);
+
+          // Atualizar o campo "completed" no Firestore com o novo valor da checkbox
+          updateDoc(taskRef, {
+            completed: checked
+          })
+            .then(() => {
+              console.log("Documento atualizado com sucesso!");
+            })
+            .catch((error) => {
+              console.error("Erro ao atualizar o documento: ", error);
+            });
+        });
+
+        const label = document.createElement('label');
+        label.setAttribute('for', taskId);
+        label.innerText = taskData.tarefa;
+
+        listItem.appendChild(checkbox);
+        listItem.appendChild(label);
+
+        list.appendChild(listItem);
+
+        // Adicionar o ID do documento ao conjunto de IDs exibidos
+        displayedTasks.add(taskId);
+      }
+    });
+  });
+}
+
+// Chamar a função displayTasks no evento onAuthStateChanged
 auth.onAuthStateChanged((user) => {
   if (user) {
     alert("Login com sucesso: " + user.displayName);
     console.log(user);
-
-    const userId = user.uid;
-    const userTasksRef = collection(db, "users");
-    const tasksQuery = query(userTasksRef, where("userId", "==", userId));
-
-    const unsubscribe = onSnapshot(tasksQuery, (snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        const taskId = change.doc.id;
-        const taskData = change.doc.data();
-
-        if (change.type === "added" && !displayedTasks.has(taskId)) {
-          const listItem = document.createElement('li');
-          listItem.classList.add('list-item');
-
-          const checkbox = document.createElement('input');
-          checkbox.setAttribute('type', 'checkbox');
-          checkbox.setAttribute('id', taskId);
-          checkbox.checked = taskData.completed;
-
-          const label = document.createElement('label');
-          label.setAttribute('for', taskId);
-          label.innerText = taskData.tarefa;
-
-          listItem.appendChild(checkbox);
-          listItem.appendChild(label);
-
-          list.appendChild(listItem);
-
-          // Adicionar o ID do documento ao conjunto de documentos exibidos
-          displayedTasks.add(taskId);
-        }
-      });
-    });
+    displayTasks();
   }
 });
 
+// Chamar a função displayTasks no evento submit do formulário
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   let tarefa = document.querySelector('[name=tarefa]').value;
@@ -82,12 +111,17 @@ form.addEventListener('submit', async (e) => {
       userId: userId
     });
     console.log("Document written with ID: ", docRef.id);
+
+    // Limpar o conjunto de IDs exibidos para evitar duplicação após adicionar uma nova tarefa
+    displayedTasks.clear();
+    displayTasks();
   } catch (e) {
     console.error("Error adding document: ", e);
   }
   alert('Registado com sucesso');
   form.reset();
 });
+
 
 
 const list = document.querySelector('.list');
