f9bab395777153647efa2066cda8ec86534c28db
Lunateka1d <bruna.marques@ubi.pt>
2023-05-29 17:36:27 +0100

    To-Do List quase feita

--- script2.js
+++ script2.js
@@ -1,8 +1,7 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -14,7 +13,7 @@ const firebaseConfig = {
     messagingSenderId: "895482491862",
     appId: "1:895482491862:web:c2a2774102183930562a77",
     measurementId: "G-YHDFPMQHB0"
-  };
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -32,8 +31,12 @@ form.addEventListener('submit', async (e) => {
     e.preventDefault();
     let tarefa = document.querySelector('[name=tarefa]').value;
     try {
+        const user = auth.currentUser;
+        const userId = user.uid;
+
         const docRef = await addDoc(collection(db, "users"), {
-            tarefa: tarefa
+            tarefa: tarefa,
+            userId: userId
         });
         console.log("Document written with ID: ", docRef.id);
     } catch (e) {
@@ -43,19 +46,40 @@ form.addEventListener('submit', async (e) => {
     form.reset();
 });
 
-auth.onAuthStateChanged((val) => {
-    if (val) {
-        alert("Login com sucesso: " + val.displayName)
-        console.log(val)
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        alert("Login com sucesso: " + user.displayName);
+        console.log(user);
+
+        const userId = user.uid;
+        const userTasksRef = collection(db, "users");
+        const tasksQuery = query(userTasksRef, where("userId", "==", userId));
+
+        const unsubscribe = onSnapshot(tasksQuery, (snapshot) => {
+            snapshot.forEach((doc) => {
+                const taskId = doc.id;
+                const taskData = doc.data();
+        
+                const listItem = document.createElement('li');
+                listItem.classList.add('list-item');
+        
+                const checkbox = document.createElement('input');
+                checkbox.setAttribute('type', 'checkbox');
+                checkbox.setAttribute('id', taskId);
+                checkbox.checked = taskData.completed;
+        
+                const label = document.createElement('label');
+                label.setAttribute('for', taskId);
+                label.innerText = taskData.tarefa;
+        
+                listItem.appendChild(checkbox);
+                listItem.appendChild(label);
+        
+                list.appendChild(listItem);
+            });
+        });
+        
     }
 });
 
 const list = document.querySelector('.list');
-
-const unsubscribe = onSnapshot(collection(db, "users"), (snapshot) => {
-    list.innerHTML = '';
-    snapshot.forEach((doc) => {
-        list.innerHTML += `${doc.data().tarefa}<br>`;
-    });
-});
-
