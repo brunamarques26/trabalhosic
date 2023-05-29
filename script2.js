1a4c837f6d0023d4d40b71a01bb6577e9a12603e
Lunateka1d <bruna.marques@ubi.pt>
2023-05-29 19:05:18 +0100

    Opção de eliminar um dos to-do list

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-import { getFirestore, collection, addDoc, onSnapshot, query, where, updateDoc,  doc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, onSnapshot, query, where, updateDoc,  doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -27,67 +27,104 @@ const db = getFirestore(app);
 
 const form = document.querySelector('form');
 
+const list = document.querySelector('.list');
+
 // Definir um conjunto para armazenar os IDs dos documentos exibidos
 const displayedTasks = new Set();
 
 // Função para exibir as tarefas
+// Função para exibir as tarefas
 function displayTasks() {
-  const user = auth.currentUser;
-  const userId = user.uid;
-  const userTasksRef = collection(db, "users");
-  const tasksQuery = query(userTasksRef, where("userId", "==", userId));
-
-  // Limpar a lista antes de exibir as tarefas novamente
-  list.innerHTML = "";
-
-  onSnapshot(tasksQuery, (snapshot) => {
-    snapshot.forEach((doc1) => {
-      const taskId = doc1.id;
-      const taskData = doc1.data();
-
-      if (!displayedTasks.has(taskId)) {
-        const listItem = document.createElement('li');
-        listItem.classList.add('list-item');
-
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type', 'checkbox');
-        checkbox.setAttribute('id', taskId);
-        checkbox.checked = taskData.completed;
-
-        // Adicionar o ouvinte de eventos ao evento 'change' da checkbox
-        checkbox.addEventListener('change', (event) => {
-          const checked = event.target.checked;
-          const taskId = event.target.id;
-
-          const taskRef = doc(db, "users", taskId);
-
-          // Atualizar o campo "completed" no Firestore com o novo valor da checkbox
-          updateDoc(taskRef, {
-            completed: checked
-          })
-            .then(() => {
-              console.log("Documento atualizado com sucesso!");
+    const user = auth.currentUser;
+    const userId = user.uid;
+    const userTasksRef = collection(db, "users");
+    const tasksQuery = query(userTasksRef, where("userId", "==", userId));
+  
+    // Limpar a lista antes de exibir as tarefas novamente
+    list.innerHTML = "";
+  
+    onSnapshot(tasksQuery, (snapshot) => {
+      snapshot.forEach((doc1) => {
+        const taskId = doc1.id;
+        const taskData = doc1.data();
+  
+        if (!displayedTasks.has(taskId)) {
+          const listItem = document.createElement('li');
+          listItem.classList.add('list-item');
+  
+          const checkbox = document.createElement('input');
+          checkbox.setAttribute('type', 'checkbox');
+          checkbox.setAttribute('id', taskId);
+          checkbox.checked = taskData.completed;
+  
+          // Adicionar o ouvinte de eventos ao evento 'change' da checkbox
+          checkbox.addEventListener('change', (event) => {
+            const checked = event.target.checked;
+            const taskId = event.target.id;
+  
+            const taskRef = doc(db, "users", taskId);
+  
+            // Atualizar o campo "completed" no Firestore com o novo valor da checkbox
+            updateDoc(taskRef, {
+              completed: checked
             })
-            .catch((error) => {
-              console.error("Erro ao atualizar o documento: ", error);
-            });
-        });
+              .then(() => {
+                console.log("Documento atualizado com sucesso!");
+              })
+              .catch((error) => {
+                console.error("Erro ao atualizar o documento: ", error);
+              });
+          });
+  
+          const label = document.createElement('label');
+          label.setAttribute('for', taskId);
+          label.innerText = taskData.tarefa;
+  
+          const deleteButton = document.createElement('button');
+          const deleteIcon = document.createElement('span');
+          deleteIcon.classList.add('material-icons');
+          deleteIcon.textContent = 'delete_forever';
+          deleteButton.classList.add('delete-button');
+          deleteButton.appendChild(deleteIcon);
+          deleteButton.addEventListener('click', () => {
+            deleteItem(taskId);
+          });
+          
 
-        const label = document.createElement('label');
-        label.setAttribute('for', taskId);
-        label.innerText = taskData.tarefa;
-
-        listItem.appendChild(checkbox);
-        listItem.appendChild(label);
-
-        list.appendChild(listItem);
-
-        // Adicionar o ID do documento ao conjunto de IDs exibidos
-        displayedTasks.add(taskId);
-      }
+          listItem.setAttribute('data-task-id', taskId); // Adicionar o atributo data-task-id
+  
+          listItem.appendChild(checkbox);
+          listItem.appendChild(label);
+          listItem.appendChild(deleteButton);
+  
+          list.appendChild(listItem);
+  
+          // Adicionar o ID do documento ao conjunto de IDs exibidos
+          displayedTasks.add(taskId);
+        }
+      });
     });
-  });
-}
+  }
+  
+  // Função para excluir uma tarefa
+  function deleteItem(taskId) {
+    const taskRef = doc(db, "users", taskId);
+    deleteDoc(taskRef)
+      .then(() => {
+        console.log("Documento excluído com sucesso!");
+  
+        // Encontrar o elemento correspondente pelo atributo data-task-id e removê-lo da lista
+        const listItem = document.querySelector(`li[data-task-id="${taskId}"]`);
+        if (listItem) {
+          listItem.remove();
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao excluir o documento: ", error);
+      });
+  }
+  
+
 
 // Chamar a função displayTasks no evento onAuthStateChanged
 auth.onAuthStateChanged((user) => {
@@ -98,7 +135,7 @@ auth.onAuthStateChanged((user) => {
   }
 });
 
-// Chamar a função displayTasks no evento submit do formulário
+//Chamar a função displayTasks no evento submit do formulário
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   let tarefa = document.querySelector('[name=tarefa]').value;
@@ -112,7 +149,7 @@ form.addEventListener('submit', async (e) => {
     });
     console.log("Document written with ID: ", docRef.id);
 
-    // Limpar o conjunto de IDs exibidos para evitar duplicação após adicionar uma nova tarefa
+    //Limpar o conjunto de IDs exibidos para evitar duplicação após adicionar uma nova tarefa
     displayedTasks.clear();
     displayTasks();
   } catch (e) {
@@ -121,7 +158,3 @@ form.addEventListener('submit', async (e) => {
   alert('Registado com sucesso');
   form.reset();
 });
-
-
-
-const list = document.querySelector('.list');
